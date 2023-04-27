1015904003e5188de5e6645eb3e148a62643ecfc
toho36 <toho0036@gmail.com>
2023-04-27 17:55:37 +0200

    139. Portal Modal
    gotta check for the typos - overlays is not overlay
    + added key so wont show error in console

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,11 +3,9 @@ import classes from './Cart.module.css';
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ key: '1', id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(
-        (item) => (
-          <li>{item.name}</li>
-        )
-      )}
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
     </ul>
   );
 
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import classes from './Modal.module.css';
 import ReactDOM from 'react-dom';
+
 const Backdrop = (props) => {
   return <div className={classes.backdrop}></div>;
 };
 const ModalOverlay = (props) => {
   return (
     <div className={classes.modal}>
-      <div className={classes.content}> {props.children}</div>
+      <div className={classes.content}>{props.children}</div>
     </div>
   );
 };
