1fa912e0222f0d1444118ca6ed64b1ec5de7b6ba
toho36 <toho0036@gmail.com>
2023-05-11 11:02:37 +0200

    fetching meals from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,34 +1,15 @@
+import { useEffect } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
 
 const AvailableMeals = () => {
+  useEffect(() => {
+    fetch(
+      'https://react-http-98ba6-default-rtdb.europe-west1.firebasedatabase.app/1.json'
+    );
+  }, []);
+
   const mealList = DUMMY_MEALS.map((meal) => (
     <MealItem
       id={meal.id}
