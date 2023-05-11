9e476ce5df593ee05edef3e87be46ae976002a3d
toho36 <toho0036@gmail.com>
2023-05-11 12:14:30 +0200

    217 Handling error using catch((error)
    cannot use try catch because of promise to async await

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,11 +5,17 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://react-http-98ba6-default-rtdb.europe-west1.firebasedatabase.app/1.json'
-      ).then();
+        'https://react-http-98ba6-default-rtdb.europe-west1.firebasedatabase.app/1.jsn'
+      );
+
+      if (!response.ok) {
+        throw new Error('something went wrong');
+      }
       const responseData = await response.json();
       const loadedMeals = [];
       for (const key in responseData) {
@@ -21,9 +27,29 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsIsLoading}>
+        <p>loading...</p>
+      </section>
+    );
+  }
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
